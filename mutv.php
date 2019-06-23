ded6c79c74ab7e9ce7a1cc41e1be6fbace57efbe
chunglop0781 <chunglop0781@gmail.com>
2019-06-23 13:19:53 +0800

    Update and rename playmedia1.php to mutv.php

---
+++ mutv.php
@@ -0,0 +1,13 @@
+
+
+
+
+<style>
+body {
+    margin-right: 0px;
+    margin-left: 0px;
+    margin-bottom: 0px;
+    margin-top: 0px;
+}
+</style>
+<iframe id="Myiframe"  class="embed-responsive-item" scrolling="no" src="http://play.nhanhtv.com/playmedia1.php?sv=https://d1wgyhp9evqv7n.cloudfront.net/hls/mutvpcweb/prog_index.m3u8" allowfullscreen="" frameborder="0" height="100%" width="100%"></iframe>
